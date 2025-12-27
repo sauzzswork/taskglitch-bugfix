351b32ed31032ecb0ffc32a50c9a3f6245148166
Saurav Mishra <sm23052003@gmail.com>
2025-12-27 13:52:36 +0530

    Expose clearLastDeleted in tasks context

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -13,8 +13,8 @@ interface TasksContextValue {
   updateTask: (id: string, patch: Partial<Task>) => void;
   deleteTask: (id: string) => void;
   undoDelete: () => void;
+  clearLastDeleted: () => void;
 }
-
 const TasksContext = createContext<TasksContextValue | undefined>(undefined);
 
 export function TasksProvider({ children }: { children: ReactNode }) {
