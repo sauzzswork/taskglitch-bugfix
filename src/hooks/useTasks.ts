6a45a85fffbea140f3180f1e36a29f584f8dbccd
Saurav Mishra <sm23052003@gmail.com>
2025-12-27 14:23:48 +0530

    Update useTasks.ts

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -22,6 +22,7 @@ interface UseTasksState {
   updateTask: (id: string, patch: Partial<Task>) => void;
   deleteTask: (id: string) => void;
   undoDelete: () => void;
+  clearLastDeleted: () => void;
 }
 
 const INITIAL_METRICS: Metrics = {
